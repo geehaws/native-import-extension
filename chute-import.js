a200ba6645e994972549d4383e5ba4e25fdfbfee
geehaws <george.haws@getchute.com>
2014-02-22 12:01:03 -0800

    attach click handler to chute icon

--- chute-import.js
+++ chute-import.js
@@ -1,16 +1,25 @@
 var chuteImport = {};
 
 chuteImport.insertImportButton = function() {
+  var chuteImport = this;
   var classNames = ['compPhoto', 'photo'];
   classNames.forEach(function(className) {
     var photoEls = document.getElementsByClassName(className);
+
     photoEls.forEach(function(photoEl) {
       var chuteIcon = document.createElement('img');
       chuteIcon.src = "//chute-icon.png";
       photoEl.appendChild(chuteIcon);
+      chuteImport.attachEventListener(chuteIcon);
     });
   });
-}
+};
+
+chuteImport.attachEventListeners = function(el) {
+  el.addEventListener('click', function() {
+    //TODO hit import endpoint
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   chuteImport.insertImportButton();
